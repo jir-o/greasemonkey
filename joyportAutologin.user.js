261497fe24289c5a0688860ee857cc301f63256f
JIRO KIKKAWA <jiro.kikkawa@gmail.com>
2012-06-13 13:05:58 +0900

    かなり修正
    GM_getValueが未定義のときにプロンプト出す記述がおかしかったので修正
    非公開ページにダイレクトで飛んだときにもログインできるように修正

--- joyportAutologin.user.js
+++ joyportAutologin.user.js
@@ -3,19 +3,18 @@
 // @namespace      http://twitter.com/jir_o
 // @description    AutoLogin for joyport
 // @include        https://joyport.inf.in.shizuoka.ac.jp/
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
 // ==/UserScript==
 
 
 (function inputID(){
-    if(GM_getValue("joyport_id") == undefined){
+    if(typeof GM_getValue("joyport_id") == 'undefined'){
 	hoge1 = prompt("input your id(ex.ia10XXX)","");
 	GM_setValue("joyport_id", hoge1);
     }
 })();
 
 (function inputPASS(){
-    if(GM_getValue("joyport_pass") == undefined){
+    if(typeof GM_getValue("joyport_pass") == 'undefined'){
 	hoge2 = prompt("input your password","");
 	GM_setValue("joyport_pass", hoge2);
     }
@@ -23,9 +22,16 @@
 
 
 
-$(document).ready(function() {
+(function() {
     document.forms.login_form_4858.elements.login_id.value=GM_getValue("joyport_id");
     document.forms.login_form_4858.elements.password.value=GM_getValue("joyport_pass");
     document.forms.login_form_4858.submit();
 })();
 
+
+//限定公開ページから遷移するログインフォームにも自動入力させる
+(function(){
+    document.forms.login_form_0.elements.login_id.value=GM_getValue("joyport_id");
+    document.forms.login_form_0.elements.password.value=GM_getValue("joyport_pass");
+    document.forms.login_form_0.submit();
+})();
